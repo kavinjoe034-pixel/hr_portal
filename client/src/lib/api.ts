ab70374d45864a0b0564c9af52154c7d5a917640
ROVE Developer <dev@rove.com>
2026-06-29 00:32:52 +0530

    client changes

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://hr-portal-3q44.onrender.com/api'
+  baseURL: 'https://hr-portal-3q44.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
